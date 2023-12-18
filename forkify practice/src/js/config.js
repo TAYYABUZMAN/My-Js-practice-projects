8c766b980eddcfa3c7b7f8c8732f1a6cea0c437c
TAYYABUZMAN <92158192+TAYYABUZMAN@users.noreply.github.com>
2023-12-18 13:52:34 -0800

    Add files via upload

---
+++ forkify practice/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8430e767-d023-4319-b8b5-6402efed7bfb';
+export const MODAL_CLOSE_SEC = 2.5;
